bdcc451dce94dfb35a786cf79c1b1c9468f673ec
root <root@yashika>
2023-06-22 14:26:19 +0530

    other files added

---
+++ models/example/my_first_dbt_model.sql
@@ -0,0 +1,29 @@
+
+/*
+    Welcome to your first dbt model!
+    
+qDid you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+SELECT * FROM `raw-data-389712.Churn.Churn_Modelling` 
+WHERE CreditScore>800 AND Age>20
+
+ 
+
+    
+
+
+
+
+
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
